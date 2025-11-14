6c92ea85cc4f2e407aba9af6a006221879094e74
kazuya kawaguchi <kawakazu80@gmail.com>
2025-11-14 23:26:44 +0900

    fix: add intlify/hono package

--- knip.config.ts
+++ knip.config.ts
@@ -4,8 +4,16 @@ export default {
   workspaces: {
     'packages/h3': {
       ignore: ['**/playground/**']
+    },
+    'packages/hono': {
+      ignore: ['**/playground/**']
     }
   },
   ignore: ['**/src/**.test-d.ts'],
-  ignoreDependencies: ['lint-staged', '@vitest/coverage-v8']
+  ignoreDependencies: [
+    'lint-staged',
+    '@vitest/coverage-v8',
+    'vitest-environment-miniflare',
+    'miniflare'
+  ]
 } satisfies KnipConfig
---
+++ packages/hono/src/index.test-d.ts
@@ -0,0 +1,20 @@
+import { expectTypeOf, test } from 'vitest'
+import { defineI18nMiddleware } from './index.ts'
+
+import type { MiddlewareHandler } from 'hono'
+
+test('defineI18nMiddleware', () => {
+  const _en = {
+    hello: 'workd'
+  }
+  // @ts-expect-error -- FIXME
+  type ResourceSchema = typeof _en // eslint-disable-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-vars -- NOTE: for type testing
+  const middleware = defineI18nMiddleware({
+    messages: {
+      en: { hello: 'world' },
+      ja: { hello: '世界' }
+    }
+  })
+
+  expectTypeOf<MiddlewareHandler>(middleware).toMatchTypeOf<MiddlewareHandler>()
+})
