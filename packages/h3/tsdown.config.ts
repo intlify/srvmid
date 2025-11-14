72e5b5f238dcd38f1a32f77be45ce3282fca4977
kazuya kawaguchi <kawakazu80@gmail.com>
2025-11-15 02:31:23 +0900

    breaking: drop cjs

---
+++ packages/h3/tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsdown'
+
+import type { UserConfig } from 'tsdown'
+
+const config: UserConfig = defineConfig({
+  entry: ['./src/index.ts'],
+  outDir: 'dist',
+  clean: true,
+  publint: true
+})
+
+export default config
