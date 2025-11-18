b9c23a1df48dc591475cfb28ff2ec6883ff61429
kazuya kawaguchi <kawakazu80@gmail.com>
2025-11-18 13:56:17 +0900

    chore: fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,7 +11,7 @@ TAG="alpha"
 for PKG in packages/* ; do
   if [[ -d $PKG ]]; then
     # Check if package is private
-    PRIVATE=$(pnpx tsx scripts/private.ts "$PKG")
+    PRIVATE=$(node scripts/private.ts "$PKG")
 
     if [[ "$PRIVATE" == "true" ]]; then
       echo "⏭️  Skipping $PKG (private package)"
